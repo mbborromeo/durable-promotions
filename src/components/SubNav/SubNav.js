b998d8b97324ad5ed3a433ebefd0dd9b0b258383
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-11-13 03:33:56 -0800

    WIP Subnav with RHS placeholder

--- src/components/SubNav/SubNav.js
+++ src/components/SubNav/SubNav.js
@@ -2,7 +2,26 @@ const SubNav = () => {
   return (
     <div id="subnav">
       <div className="desktop-border">
-        <h1>Marketing</h1>
+        <div className="flex-wrapper">
+          <div className="flex-wrapper-left">
+            <h1>Marketing</h1>
+            <ul>
+              <li class="on">
+                <a href="#">Promotions</a>
+              </li>
+              <li>
+                <a href="#">Brand</a>
+              </li>
+              <li>
+                <a href="#">Reviews</a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-wrapper-right">
+            <a href="#">Notifications</a>
+            <a href="#">Profile</a>
+          </div>
+        </div>
       </div>
     </div>
   );
