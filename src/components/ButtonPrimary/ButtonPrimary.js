f30926bc116c77e67e52298dbd4747aa13b55c3d
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-11-14 01:03:51 -0800

    WIP Listing actions: search field expands on focus

--- src/components/ButtonPrimary/ButtonPrimary.js
+++ src/components/ButtonPrimary/ButtonPrimary.js
@@ -2,7 +2,7 @@ const ButtonPrimary = ({ label, handleClick }) => {
   return (
     <button
       type="button"
-      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 leading-none h-11 md:h-9 px-4 py-3.5 md:py-2 text-sm font-medium text-white hover:bg-indigo-50 hover:text-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+      className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 leading-none w-full md:w-40 h-11 md:h-9 px-4 py-3.5 md:py-2 text-sm font-medium text-white hover:bg-indigo-50 hover:text-indigo-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
       onClick={handleClick}
     >
       {label}
