911b35cae982a40ebeabe376e74df9f60175e20b
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-11-16 14:35:23 -0800

    WIP Listing Cards layout

--- src/components/PromotionCard/PromotionCard.js
+++ src/components/PromotionCard/PromotionCard.js
@@ -3,17 +3,23 @@ import "./PromotionCard.css";
 
 const PromotionCard = ({ promotion, index }) => {
   return (
-    <div>
-      <span className={`badge ${promotion.type}`}></span>
-      <h1>{promotion.type}</h1>
-      <div>{moment(promotion.timestamp).format("ll")}</div>
+    <div className="card">
+      <div className="flex-row">
+        <span className="heading">{promotion.type}</span>
+        <span className={`badge ${promotion.type}`}></span>
+      </div>
+      <div className="date">{moment(promotion.timestamp).format("ll")}</div>
       <div>{promotion.url}</div>
       <h2>{promotion.company}</h2>
       <p>{promotion.paragraph}</p>
-      <a href="#" className="icon_view">
-        View
-      </a>
-      <hr />
+      <div className="links">
+        <a href="#" className="view">
+          View
+        </a>
+        <a href="#" className="more">
+          More
+        </a>
+      </div>
     </div>
   );
 };
