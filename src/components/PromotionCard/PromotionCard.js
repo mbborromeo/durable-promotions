8a649b23363104cf998f8be56f1e506139fb2ee2
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-11-16 16:41:47 -0800

    list preview thumbs + border-radius

--- src/components/PromotionCard/PromotionCard.js
+++ src/components/PromotionCard/PromotionCard.js
@@ -1,3 +1,4 @@
+// import Image from "next/image";
 import moment from "moment";
 import "./PromotionCard.css";
 
@@ -9,9 +10,15 @@ const PromotionCard = ({ promotion, index }) => {
         <span className={`badge ${promotion.type}`}></span>
       </div>
       <div className="date">{moment(promotion.timestamp).format("ll")}</div>
-      <div>{promotion.url}</div>
+      <div className="preview">
+        <img
+          src={`/images/preview_${promotion.type}.png`}
+          alt={`preview thumb ${promotion.type}`}
+        />
+      </div>
+      {/* <div>{promotion.url}</div>
       <h2>{promotion.company}</h2>
-      <p>{promotion.paragraph}</p>
+      <p>{promotion.paragraph}</p> */}
       <div className="links">
         <a href="#" className="view">
           View
