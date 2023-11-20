712e52123617282eb5e457ded542ab83839a1767
Michael Borromeo <michael.borromeo@digitalistgroup.com>
2023-11-20 04:30:47 -0800

    remove some comments

--- src/app/marketing/page.js
+++ src/app/marketing/page.js
@@ -87,6 +87,7 @@ export default function Marketing() {
         <div className="flex-wrapper-column">
           {searchAndFilteredPromotions.length > 0 &&
             searchAndFilteredPromotions.map((promo, p) => {
+              console.log("Marketing promo", promo);
               return (
                 <PromotionCard
                   promotion={promo}
